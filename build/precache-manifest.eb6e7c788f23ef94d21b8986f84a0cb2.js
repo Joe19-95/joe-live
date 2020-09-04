6843c638d7c8b1bc261bd235ebeee018d4b1116d
joe <joejan19@gmail.com>
2020-09-04 20:19:04 +0530

    please add this too

---
+++ build/precache-manifest.eb6e7c788f23ef94d21b8986f84a0cb2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dbe47089f4a0ccb34c3095cbfa6f6d19",
+    "url": "/joe-live/index.html"
+  },
+  {
+    "revision": "8fad0f3034ce83415d24",
+    "url": "/joe-live/static/css/main.52208b0c.chunk.css"
+  },
+  {
+    "revision": "bbc3ce5694fd116f2cad",
+    "url": "/joe-live/static/js/2.10d4f435.chunk.js"
+  },
+  {
+    "revision": "d0b13a04dd0dc4c851ad16fad1cc6f51",
+    "url": "/joe-live/static/js/2.10d4f435.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8fad0f3034ce83415d24",
+    "url": "/joe-live/static/js/main.56fd6295.chunk.js"
+  },
+  {
+    "revision": "325532f9e2d464fb0e30",
+    "url": "/joe-live/static/js/runtime-main.3b2aad0a.js"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/joe-live/static/media/cart.30f07071.svg"
+  },
+  {
+    "revision": "19ef7b17bc9414c63c7781251ce0d24d",
+    "url": "/joe-live/static/media/crown.19ef7b17.svg"
+  }
+]);
